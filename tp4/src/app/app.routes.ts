2a6cf1b7de3c8e7cfa8d9b37243b68c1cd449ca3
Joanna Raveloson <joanna.raveloson.etu@unistra.fr>
2025-11-11 23:32:49 +0100

    TP4 Ajout gestion des utilisateurs

--- tp4/src/app/app.routes.ts
+++ tp4/src/app/app.routes.ts
@@ -3,12 +3,15 @@ import { ListePollutionsComponent } from './liste-pollutions/liste-pollutions.co
 import { DetailsPollutionComponent } from './details-pollution/details-pollution.component';
 import { AddPollutionComponent } from './add-pollution/add-pollution.component';
 import { EditPollutionComponent } from './edit-pollution/edit-pollution.component';
+import { AddUtilisateurComponent } from './add-utilisateur/add-utilisateur.component';
+import { ListeUtilisateursComponent } from './liste-utilisateurs/liste-utilisateurs.component';
 
 export const routes: Routes = [
     { path: '', redirectTo: 'pollutions', pathMatch: 'full' },
     { path: 'pollutions', component: ListePollutionsComponent },
     { path: 'pollutions/add', component: AddPollutionComponent },
     { path: 'pollutions/:id', component: DetailsPollutionComponent },
-    { path: 'pollutions/edit/:id', component: EditPollutionComponent }
-
+    { path: 'pollutions/edit/:id', component: EditPollutionComponent },
+    { path: 'utilisateurs/add', component: AddUtilisateurComponent },
+    { path: 'utilisateurs', component: ListeUtilisateursComponent }
 ];
